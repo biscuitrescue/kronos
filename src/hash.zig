c4a67d7d26ccd06a08e145ff4ebcd6dfc338c062
biscuitrescue <karttikeyasinha11@gmail.com>
2025-12-26 12:50:34 +0530

    parser

--- src/hash.zig
+++ src/hash.zig
@@ -2,16 +2,16 @@ const std = @import("std");
 const blake3 = std.crypto.hash.Blake3;
 
 // 32 -> Blake3
-pub const Hash = [32]u8;
 
-pub fn hasher(data: []const u8) Hash {
-    var out: Hash = undefined;
-    blake3.hash(data, &out, .{});
-    return out;
-}
+pub fn main() !void {
+    var hasher = blake3.Hasher.init(std.heap.page_allocator);
+    defer hasher.deinit(); // Crucial for resource management
 
-test "determ" {
-    const h1 = blake3("hello");
-    const h2 = blake3("hello");
-    try std.testing.expect(std.mem.eql(u8, &h1, &h2));
+const data = try std.fmt.allocBufferToString(std.heap.page_allocator, "hello zig");
+    defer std.heap.page_allocator.free(data.ptr); // Free allocated data
+
+try hasher.update(data);
+    const hash = hasher.finalize();
+
+try std.io.getStdOut().writer().print("BLAKE3 hash: {}\n", .{hash});
 }
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,30 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
+
+const Config = struct {
+    abs_path: []const u8,
+    mount_path: []const u8,
+    wal_path: []const u8,
+    merkle_path: []const u8,
+    snap_path: []const u8,
+    store_path: []const u8,
+    cache_size: usize,
+    chunk_size: usize,
+};
+
+fn parse_config(alloc: Allocator, args: anytype) !Config {
+    return Config {
+        .abs_path = try alloc.dupe(u8, args.abs),
+        .mount_path = try alloc.dupe(u8, args.mount),
+        .wal_path = try std.fs.path.join(alloc, &.{ args.abs, ".kronos/wal"}),
+        .merkle_path = try std.fs.path.join(alloc, &.{ args.abs, ".kronos/merkle.idx"}),
+        .snap_path = try std.fs.path.join(alloc, &.{ args.abs, ".kronos/snap"}),
+        .store_path = try std.fs.path.join(alloc, &.{ args.abs, ".kronos/chunks"}),
+        .cache_size = args.cache orelse 512,
+        .chunk_size = 64 * 1024,
+    };
+}
 
 pub fn main() !void {
-    
+
 }
