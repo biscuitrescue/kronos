0f9f5bedb91ca402bb8738ed5d1025e7703235ab
biscuitrescue <karttikeyasinha11@gmail.com>
2025-12-27 12:08:29 +0530

    ;(

---
+++ src/sandbox.zig
@@ -0,0 +1,9 @@
+const std = @import("std");
+
+const SandboxConfig = struct {
+    id: []const u8,
+    root_path: []const u8,
+    allow_netw: bool,
+    read_only_paths: [][]const u8,
+    hidden_paths: [][]const u8,
+};
