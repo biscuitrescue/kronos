8304da02be9e8d16f2dae681e3273ec3baa8d1ed
biscuitrescue <karttikeyasinha11@gmail.com>
2025-12-29 22:57:49 +0530

    sandbox init

--- src/sandbox.zig
+++ src/sandbox.zig
@@ -10,19 +10,30 @@ const SandboxConfig = struct {
 };
 
 pub const SandBox = struct {
-    pub fn run(alloc: std.mem.Allocator, cfg: SandBox, argv: []const []const u8) !void {
-        // Error on non linux/win (mac)
+    pub fn run(alloc: std.mem.Allocator, cfg: SandboxConfig, argv: []const []const u8) !void {
+        // unnecessary exec on non linux / win machines
+
+        try std.os.linux.unshare(std.os.linux.CLONE.NEWNS);
+        try std.os.chdir(cfg.root_path);
+        try std.os.linux.chroot(cfg.root_path);
+
         var child = std.process.Child.init(argv, alloc);
         child.cwd = cfg.root_path;
-        child.env_map = std.process.EnvMap.init(alloc);
+
+        var env = std.process.EnvMap.init(alloc);
+        child.env_map = &env;
 
         switch (builtin.os.tag) {
             .linux => try child.env_map.put("PATH", "/usr/bin"),
             .windows => try child.env_map.put("PATH", "C:\\Windows\\System32"),
-            else => return std.log.err("undefined OS", .{}),
+            else => return error.UnsupportedOS,
         }
 
         try child.spawn();
-        _ = try child.wait();
+        const term = try child.wait();
+
+        if (term != .Exited or term.Exited != 0) {
+            std.log.err("Child process exited with code {}", .{term.Exited.code});
+        }
     }
 };
