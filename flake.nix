8c3311095e37203932234a25dcfc814cb0e78ad0
biscuitrescue <karttikeyasinha11@gmail.com>
2026-03-01 22:16:34 +0530

    envars

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "A Nix-flake-based C/C++ development environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0"; # stable Nixpkgs
+
+  outputs =
+    { self, ... }@inputs:
+
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import inputs.nixpkgs { inherit system; };
+          }
+        );
+
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default =
+            pkgs.mkShell.override
+              {
+                # Override stdenv in order to change compiler:
+                # stdenv = pkgs.clangStdenv;
+              }
+              {
+                packages =
+                  with pkgs;
+                  [
+                    zlib
+                    stdenv.cc.cc.lib
+                    pkg-config
+                    openssl
+                    nix-ld
+                  ];
+                LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+                  pkgs.zlib
+                  pkgs.stdenv.cc.cc.lib
+                  pkgs.openssl
+                ];
+              };
+        }
+      );
+    };
+}
